98f62fa60b680263e4073df5a96a9e4c4b273b1f
DESKTOP-DOGVPUB\SSAFY <caca30@naver.com>
2023-10-19 17:55:22 +0900

    Feat : 포스트 내용 추가

--- src/template/post.jsx
+++ src/template/post.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Img from "gatsby-image"; // <-- 추가
+import Img from "gatsby-image";
 
 export default function post1({ data }) {
   const post = data.markdownRemark;
@@ -8,7 +8,8 @@ export default function post1({ data }) {
   return (
     <div>
       <h1>{post.frontmatter.title}</h1>
-      {post.frontmatter.image && ( // <-- 이미지가 있는 경우만 렌더링
+      <p>{post.frontmatter.date}</p>
+      {post.frontmatter.image && ( // 이미지가 있는 경우만 렌더링
         <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
       )}
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -22,6 +23,7 @@ export const query = graphql`
       html
       frontmatter {
         title
+        date(formatString: "YYYY-MM-DD")
         image {
           childImageSharp {
             fluid(maxWidth: 800) {
